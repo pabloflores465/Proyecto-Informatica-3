033eb98e89f533e56fad6128abcace70a437b065
Pablo Flores <pablopolis2016@gmail.com>
2023-03-03 08:17:33 -0600

    añadiendo clase documentacion

---
+++ documentacion.cpp
@@ -0,0 +1,6 @@
+#include "documentacion.h"
+
+documentacion::documentacion()
+{
+
+}
---
+++ documentacion.h
@@ -0,0 +1,11 @@
+#ifndef DOCUMENTACION_H
+#define DOCUMENTACION_H
+
+
+class documentacion
+{
+public:
+    documentacion();
+};
+
+#endif // DOCUMENTACION_H
