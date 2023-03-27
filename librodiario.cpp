c3925a834829be84e0f921e34d0b518d93444128
Jose Alonzo <josalonzo.gt@gmail.com>
2023-03-27 15:57:52 -0600

    Corrección de Errores de la clase "libro diario" y actualización del formulario

--- librodiario.cpp
+++ librodiario.cpp
@@ -1,70 +1,70 @@
 #include "librodiario.h"
 
 //Metodos set de los indices de las cuentas
-void setCuenta1(int primera)
+void libroDiario::setCuenta1(int primera)
 {
     this->cuenta1=primera;
 }
-void setCuenta2(int segunda)
+void libroDiario::setCuenta2(int segunda)
 {
     this->cuenta2=segunda;
 }
-void setCuenta3(int tercera)
+void libroDiario::setCuenta3(int tercera)
 {
     this->cuenta3=tercera;
 }
-void setCuenta4(int cuarta)
+void libroDiario::setCuenta4(int cuarta)
 {
     this->cuenta4=cuarta;
 }
 //Metodos get de los indices de las cuentas
-int getCuenta1()
+int libroDiario::getCuenta1()
 {
     return this->cuenta1;
 }
-int getCuenta2()
+int libroDiario::getCuenta2()
 {
     return this->cuenta2;
 }
-int getCuenta3()
+int libroDiario::getCuenta3()
 {
     return this->cuenta3;
 }
-int getCuenta4()
+int libroDiario::getCuenta4()
 {
     return this->cuenta4;
 }
 //Metodos set de los valores de las cuentas
-void setValor1(int first)
+void libroDiario::setValor1(int first)
 {
     this->valor1=first;
 }
-void setValor2(int second)
+void libroDiario::setValor2(int second)
 {
     this->valor2=second;
 }
-void setValor3(int third)
+void libroDiario::setValor3(int third)
 {
     this->valor3=third;
 }
-void setValor4(int fourth)
+void libroDiario::setValor4(int fourth)
 {
     this->valor4=fourth;
 }
 //Metodos get de los valores de las cuentas
-double getValor1()
+double libroDiario::getValor1()
 {
     return this->valor1;
 }
-double getValor2()
+double libroDiario::getValor2()
 {
     return this->valor2;
 }
-double getValor3()
+double libroDiario::getValor3()
 {
     return this->valor3;
 }
-double getValor4()
+double libroDiario::getValor4()
 {
     return this->valor4;
 }
