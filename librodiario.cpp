0dedbd3a8c5d3c87274ae9bdd5760bf52e8538c5
Jose Alonzo <josalonzo.gt@gmail.com>
2023-03-27 15:26:20 -0600

    Actualización de la Clase "libro diario"

--- librodiario.cpp
+++ librodiario.cpp
@@ -1,6 +1,70 @@
 #include "librodiario.h"
 
-libroDiario::libroDiario()
+//Metodos set de los indices de las cuentas
+void setCuenta1(int primera)
 {
-
+    this->cuenta1=primera;
+}
+void setCuenta2(int segunda)
+{
+    this->cuenta2=segunda;
+}
+void setCuenta3(int tercera)
+{
+    this->cuenta3=tercera;
+}
+void setCuenta4(int cuarta)
+{
+    this->cuenta4=cuarta;
+}
+//Metodos get de los indices de las cuentas
+int getCuenta1()
+{
+    return this->cuenta1;
+}
+int getCuenta2()
+{
+    return this->cuenta2;
+}
+int getCuenta3()
+{
+    return this->cuenta3;
+}
+int getCuenta4()
+{
+    return this->cuenta4;
+}
+//Metodos set de los valores de las cuentas
+void setValor1(int first)
+{
+    this->valor1=first;
+}
+void setValor2(int second)
+{
+    this->valor2=second;
+}
+void setValor3(int third)
+{
+    this->valor3=third;
+}
+void setValor4(int fourth)
+{
+    this->valor4=fourth;
+}
+//Metodos get de los valores de las cuentas
+double getValor1()
+{
+    return this->valor1;
+}
+double getValor2()
+{
+    return this->valor2;
+}
+double getValor3()
+{
+    return this->valor3;
+}
+double getValor4()
+{
+    return this->valor4;
 }
--- librodiario.h
+++ librodiario.h
@@ -4,8 +4,38 @@
 
 class libroDiario
 {
+private:
+    //atributos de los indices de las cuentas
+    int cuenta1;
+    int cuenta2;
+    int cuenta3;
+    int cuenta4;
+    //atributos de los valores de las cuentas
+    double valor1;
+    double valor2;
+    double valor3;
+    double valor4;
 public:
-    libroDiario();
-};
+    //Metodos set de los indices de las cuentas
+    void setCuenta1(int primera);
+    void setCuenta2(int segunda);
+    void setCuenta3(int tercera);
+    void setCuenta4(int cuarta);
+    //Metodos get de los indices de las cuentas
+    int getCuenta1();
+    int getCuenta2();
+    int getCuenta3();
+    int getCuenta4();
+    //Metodos set de los valores de las cuentas
+    void setValor1(int first);
+    void setValor2(int second);
+    void setValor3(int third);
+    void setValor4(int fourth);
+    //Metodos get de los valores de las cuentas
+    double getValor1();
+    double getValor2();
+    double getValor3();
+    double getValor4();
+
 
 #endif // LIBRODIARIO_H
